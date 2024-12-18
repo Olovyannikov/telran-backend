03697244247a75cac0cb262900adefd3c6e72f57
Ilya Olovyannikov <54705024+Olovyannikov@users.noreply.github.com>
2024-12-18 14:41:07 +0300

    fix(sale): add correct route data
    - Добавляет выборку продуктов со скидкой по параметру "discont_price"

--- routes/sale.js
+++ routes/sale.js
@@ -1,17 +1,24 @@
-const { request } = require('express');
 const express = require('express');
+const { Op } = require("sequelize");
+
+const Product = require("../database/models/product");
 
 const router = express.Router();
 
+router.get('/',async (req, res) =>{
+    const products = await Product.findAll({
+        where: {
+            discont_price: {
+                [Op.gt]: 0
+            }
+        }
+    });
 
-router.get('/send', (req, res) =>{
-    res.json({});
-
+    return res.json(products);
 })
 
-router.post('/send', (req, res) => {
-    
-    res.json({status: 'OK', message: 'request processed'})
+router.post('/', (req, res) => {
+    return res.json({status: 'OK', message: 'request processed'})
 })
 
 
